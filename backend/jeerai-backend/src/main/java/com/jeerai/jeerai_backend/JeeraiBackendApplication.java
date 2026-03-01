8f61ad69e3dc9d3d3a506742f99c0612767b6439
Vraj911 <vraj9112005@gmail.com>
2026-03-01 23:41:54 +0530

    backend structure ready

---
+++ backend/jeerai-backend/src/main/java/com/jeerai/jeerai_backend/JeeraiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jeerai.jeerai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeeraiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeeraiBackendApplication.class, args);
+	}
+
+}
---
+++ backend/jeerai-backend/src/test/java/com/jeerai/jeerai_backend/JeeraiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeerai.jeerai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeeraiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
